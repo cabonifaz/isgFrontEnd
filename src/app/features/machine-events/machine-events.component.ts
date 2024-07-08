4d6c7f61f5f161b4c47b112222e8c175e131403b
Oliver Castillo <olcastillot@gmail.com>
2024-07-07 20:13:54 -0500

    feat: add table of machine events

--- src/app/features/machine-events/machine-events.component.ts
+++ src/app/features/machine-events/machine-events.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Machine} from "../../shared/models/machine.interface";
 import {ActivatedRoute} from "@angular/router";
 import {MACHINES} from "../../mocks/mock-data";
+import {saveAs} from "file-saver";
 
 @Component({
   selector: 'app-machine-events',
@@ -22,4 +23,21 @@ export class MachineEventsComponent implements OnInit {
     });
   }
 
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.machine.events);
+      const workbook = {Sheets: {'data': worksheet}, SheetNames: ['data']};
+      const excelBuffer: any = xlsx.write(workbook, {bookType: 'xlsx', type: 'array'});
+      this.saveAsExcelFile(excelBuffer, "products");
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
 }
