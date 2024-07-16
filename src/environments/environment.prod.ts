746d8a754c7497e12367fc9e1397f55a5e9ee391
Oliver Castillo <olcastillot@gmail.com>
2024-07-16 12:17:48 -0500

    fix: modify table design

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
+  //DB -> staging
   url: 'https://jazi2uqtnj.execute-api.us-east-2.amazonaws.com'
+  //DB -> isg_preprod
+  //url: 'https://d0z1siqns0.execute-api.us-east-2.amazonaws.com'
 };
