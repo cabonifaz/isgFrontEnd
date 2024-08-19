3a4393d54b193bcd7502d21e38274194baedc93d
Brandt322 <bgamboaa98@gmail.com>
2024-08-19 14:42:24 -0500

    feat: build to prod

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/jazi'),
+      dir: require('path').join(__dirname, './coverage/isg'),
       subdir: '.',
       reporters: [
         { type: 'html' },
