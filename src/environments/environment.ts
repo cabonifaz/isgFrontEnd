7f7d588b67152dd06cde0e21d2b0ea8218cf8b64
Brandt322 <bgamboaa98@gmail.com>
2024-10-02 11:50:16 -0500

    feat: Change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   //DB -> staging
   // url: 'https://jazi2uqtnj.execute-api.us-east-2.amazonaws.com'
   //DB -> isg_preprod
-  //url: 'https://d0z1siqns0.execute-api.us-east-2.amazonaws.com'
+  url: 'https://d0z1siqns0.execute-api.us-east-2.amazonaws.com'
   //DB -> prod
-  url: 'https://i6qy8cjk3j.execute-api.us-east-1.amazonaws.com'
+  // url: 'https://i6qy8cjk3j.execute-api.us-east-1.amazonaws.com'
 };
 
 /*
